7e0a346a84d6d2bf448547cecc36e92c2f626a07
Matthew Reider <matthew.reider@dynatrace.com>
2024-03-09 22:05:18 +0100

    dd

--- setup.sh
+++ setup.sh
@@ -1,34 +1,25 @@
-#!/bin/bash
-snap install docker
-snap install microk8s --classic --channel=1.28/stable
-microk8s enable registry
 # Ask for Dynatrace endpoint and token
 read -p "Enter your Dynatrace tenant (ex: live.dt.com): " DT_ENDPOINT
 read -p "Enter your Dynatrace token: " DT_TOKEN
 # Containerize using Docker
 echo "Building"
-docker build -t backend:latest ./apps/backend
-docker build -t frontend:latest ./apps/frontend
-docker build -t proxy:latest ./apps/proxy
-docker tag backend:latest localhost:32000/backend:latest
-docker tag frontend:latest localhost:32000/frontend:latest
-docker tag proxy:latest localhost:32000/proxy:latest
+docker build -t localhost:5000/backend:v1.0 ./apps/backend
+docker build -t localhost:5000/frontend:v1.0 ./apps/frontend
+docker build -t localhost:5000/proxy:v1.0 ./apps/proxy
 # Push to local registry
 echo "Pushing to local registry"
-docker push localhost:32000/backend:latest
-docker push localhost:32000/frontend:latest
-docker push localhost:32000/proxy:latest
-# Create secret for Dynatrace
-echo "Creating Kubernetes secret for Dynatrace credentials..."
-microk8s.kubectl create secret generic dynatrace-otelcol-dt-api-credentials -n orders --from-literal=DT_API_ENDPOINT="https://$DT_ENDPOINT" --from-literal=DT_API_TOKEN="$DT_TOKEN"
+podman push localhost:5000/backend:v1.0 --tls-verify=false
+podman push localhost:5000/frontend:v1.0 --tls-verify=false
+podman push localhost:5000/proxy:v1.0 --tls-verify=false
 # Deploy
 echo "Deploying"
-microk8s.kubectl  apply -f deployments/namespace-orders.yaml
-microk8s.kubectl  apply -f deployments/deployment-dynatrace-collector.yaml
-microk8s.kubectl  apply -f deployments/deployment-backend.yaml
-microk8s.kubectl  apply -f deployments/deployment-frontend.yaml
-microk8s.kubectl  apply -f deployments/deployment-proxy.yaml
-microk8s.kubectl  set env deployment/backend -n orders UPDATE_TIME="$(date)"
-microk8s.kubectl  set env deployment/frontend -n orders UPDATE_TIME="$(date)"
-microk8s.kubectl  set env deployment/proxy -n orders UPDATE_TIME="$(date)"
+kubectl  apply -f deployments/namespace-orders.yaml
+kubectl create secret generic dynatrace-otelcol-dt-api-credentials -n orders --from-literal=DT_API_ENDPOINT="https://$DT_ENDPOINT" --from-literal=DT_API_TOKEN="$DT_TOKEN"
+kubectl  apply -f deployments/deployment-dynatrace-collector.yaml
+kubectl  apply -f deployments/deployment-backend.yaml
+kubectl  apply -f deployments/deployment-frontend.yaml
+kubectl  apply -f deployments/deployment-proxy.yaml
+kubectl  set env deployment/backend -n orders UPDATE_TIME="$(date)"
+kubectl  set env deployment/frontend -n orders UPDATE_TIME="$(date)"
+kubectl  set env deployment/proxy -n orders UPDATE_TIME="$(date)"
 echo "Setup complete."
